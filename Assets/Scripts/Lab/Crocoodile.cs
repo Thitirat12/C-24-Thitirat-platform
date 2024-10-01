d9dc19a5c8b55e958523011dc26b7cdac2202562
Thitirat12 <thitirat.phua@bumail.net>
2024-10-01 21:16:14 +0700

    Main

---
+++ Assets/Scripts/Lab/Crocoodile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crocoodile : Enemy
+{
+    public override void Behavior()
+    {
+        Debug.Log("Crocoodile.Behavior");
+    }
+}

416b94b5b8841fa4d2bff20e27d09e1a00598a2d
Thitirat12 <thitirat.phua@bumail.net>
2024-10-02 16:19:43 +0700

    AntWalk

--- Assets/Scripts/Lab/Ant.cs
+++ Assets/Scripts/Lab/Ant.cs
@@ -4,8 +4,50 @@ using UnityEngine;
 
 public class Ant : Enemy
 {
+
+    [SerializeField] private Vector2 velocity;
+    [SerializeField] private Transform[] movePoints;
+
+
+    private void Start()
+    {
+        //Initialize(10);
+        Debug.Log($"{Health}");
+        Behavior();
+    }
+
+    private void FixedUpdate()
+    {
+        Behavior();  
+    }
+
+    /*public void Initialize(int newHealth)
+    {
+        Health = newHealth;
+    }*/
+
     public override void Behavior()
     {
-        Debug.Log("Ant.Behavior");
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+        if (rb.position.x <= movePoints[0].position.x && velocity.x < 0)
+        {
+            FlipCharacter();
+        }
+
+        else if (rb.position.x >= movePoints[1].position.x && velocity.x > 0)
+        {
+            FlipCharacter();
+        }
+
+    }
+
+    private void FlipCharacter()
+    {
+        velocity *= -1;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
--- Assets/Scripts/Lab/Character.cs
+++ Assets/Scripts/Lab/Character.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public abstract class Character : MonoBehaviour
 {
-    private int health;
+    
+    [SerializeField] private int health;
     public int Health
     {
-        get
-        {
-            return health;
-        }
-        set
-        {
-            health = value;
-        }
+        get { return health; }
+        set { health = value; }
     }
 
-    protected Animator anim;
-    protected Rigidbody2D rb;
+    public Animator anim;
+    public Rigidbody2D rb;
 
     public bool IsDead()
     {

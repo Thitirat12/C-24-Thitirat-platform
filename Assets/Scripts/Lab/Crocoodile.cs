bb545222a57c81c44d7459af27de737783c20346
Thitirat12 <thitirat.phua@bumail.net>
2024-10-02 17:14:31 +0700

    Croc

--- Assets/Scripts/Lab/Crocoodile.cs
+++ Assets/Scripts/Lab/Crocoodile.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 public class Crocoodile : Enemy
 {
+
+    [SerializeField] private float attackRange;
+    public Player player;
+
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawnPoint;
+
+
+    [SerializeField] private float bulletSpawnTime;
+    [SerializeField] private float bulletTimer;
+
+
+    private void Update()
+    {
+        bulletTimer -= Time.deltaTime;
+
+        Behavior();
+    }
+
     public override void Behavior()
     {
-        Debug.Log("Crocoodile.Behavior");
+        Vector3 dicrection = player.transform.position - transform.position;
+        float distance = dicrection.magnitude;
+
+        if (distance < attackRange)
+        {
+            Shoot();
+        }
     }
+
+    private void Shoot()
+    {
+        if (bulletTimer <= 0)
+        {
+            Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
+
+            bulletTimer = bulletSpawnTime;
+        } 
+    }
+
 }
